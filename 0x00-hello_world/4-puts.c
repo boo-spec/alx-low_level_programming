f8e02cb2b9fac0e5f3885d5100e9afcdc0d0ad5a
boo-spec <ifeomaglory76@gmail.com>
2022-09-08 02:55:17 -0700

    adding 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
-/**
- *  *main - entry point
- *   *
- *    *Return: Always 0 (success)
- 
-  */
+     /**
+     *main - entry point
+     *
+     *Return: Always 0 (success)
+      */ 
 
 int main(void)
 {
