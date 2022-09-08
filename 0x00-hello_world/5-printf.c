86320bf9b64ad0d09f5997a3649ea0c3d8c4d9a3
boo-spec <ifeomaglory76@gmail.com>
2022-09-08 03:32:17 -0700

    adding 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,10 @@
-      #include <stdio.h>
-      /**
-      *main - entry
-      *
-      *Return: Always 0 (success)
-      *
-      */
+#include <stdio.h>
+/**
+*main - entry
+*
+*Return: Always 0 (success)
+*
+*/
 int main(void)
 {
        	printf("with proper grammar, but the outcome is a piece of t,\n");
