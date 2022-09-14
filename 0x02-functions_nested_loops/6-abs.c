d7d6a222250366d29e7719fad8a74e219745d40a
boo-spec <ifeomaglory76@gmail.com>
2022-09-14 16:15:14 -0700

    adding myfiles

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -1,26 +1,20 @@
-#include "main.h"
-#include <stdio.h>
-
-/**
- * returns an absolute value of an int
- *
- * @n: this file provides the values needed
- *
- * Return: 0;
+/*
+ * File: 6-abs.c
+ * Auth: Brennan D Baraban
  */
 
+#include "main.h"
+
+/**
+ * _abs - Computes the absolute value of an integer.
+ * @n: The integer to be computed.
+ *
+ * Return: The absolute value of the integer.
+ */
 int _abs(int n)
-
 {
-
-	if (n < 0)
-
-	{
-
-		return (n * -1);
-
-	}
-
-	return (n);
-
+	if (n >= 0)
+		return (n);
+	else
+		return (-n);
 }
