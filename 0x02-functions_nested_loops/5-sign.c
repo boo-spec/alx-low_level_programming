c584b5be8754190e5f6ac1e2bc9fb9eca0392815
boo-spec <ifeomaglory76@gmail.com>
2022-09-14 14:02:29 -0700

    adding myfiles

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,33 +1,49 @@
 #include "main.h"
 
+
+
 /**
- * main - check the code.
+ *
+ *print_sign - prints the sign of a number
+ *
+ *@n: number being tested
  *
  * Return: Always 0.
+ *
  */
-int main(void)
-{
-    int r;
 
-    r = print_sign(98);
-    _putchar(',');
-    _putchar(' ');
-    _putchar(r + '0');
-    _putchar('\n');
-    r = print_sign(0);
-    _putchar(',');
-    _putchar(' ');
-    _putchar(r + '0');
-    _putchar('\n');
-    r = print_sign(0xff);
-    _putchar(',');
-    _putchar(' ');
-    _putchar(r + '0');
-    _putchar('\n');
-    r = print_sign(-1);
-    _putchar(',');
-    _putchar(' ');
-    _putchar(r + '0');
-    _putchar('\n');
-    return (0);
+int print_sign(int n)
+{
+
+	if (n > 0)
+
+:wq
+{
+
+		_putchar(43);
+
+		return (1);
+
+	}
+
+	else if (n == 0)
+
+	{
+
+		_putchar(48);
+
+		return (0);
+
+	}
+
+	else
+
+	{
+
+		_putchar(45);
+
+		return (-1);
+
+	}
+
 }
