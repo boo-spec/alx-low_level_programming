beebe6a77a00cd54dbd0efd550481572f49251a6
boo-spec <ifeomaglory76@gmail.com>
2022-09-19 09:48:08 -0700

    c

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,23 @@
+void print_square(int size);                                                                                                                           
+                                                                                                                                                       
+                                                                                                                                                       
+                                                                                                                                                       
+void print_triangle(int size);                                                                                                                         
+                                                                                                                                                       
+                                                                                                                                                       
+                                                                                                                                                       
+int isPrime(long int n);                                                                                                                               
+                                                                                                                                                       
+                                                                                                                                                       
+                                                                                                                                                       
+long int biggestFactor(long int a);                                                                                                                    
+                                                                                                                                                       
+                                                                                                                                                       
+                                                                                                                                                       
+void print_number(int n);                                                                                                                              
+                                                                                                                                                       
+                                                                                                                                                       
+                                                                                                                                                       
+int numDigits(int n);                                                                                                                                  
+                                                                                                                                                       
+#endif                      
