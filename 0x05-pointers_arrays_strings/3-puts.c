a7b78abced12408a8a97608382b7d944e40962ee
boo-spec <ifeomaglory76@gmail.com>
2022-09-20 01:52:57 -0700

    c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,22 +1,17 @@
 #include "main.h"
-
 /**
- * '_puts': - prints a string, followed by a new line.
- * @str: input string.
- * Return: no return.
+ *_puts- prints out to stdout with new line
+ *@str: checked
+ *Return:nothing
  */
 void _puts(char *str)
 {
-	int count = 0;
-
-	while (count >= 0)
+	if (str)
 	{
-		if (str[count] == '\0')
+		while (*str)
 		{
-			_putchar('\n');
-			break;
+			_putchar(*str++);
 		}
-		'_putchar'(str[count]);
-		count++;
 	}
+	_putchar('\n');
 }
